3d00cdbfe9e00aebb943f933f9fe797fcc56718e
ishmamt <55696609+ishmamt@users.noreply.github.com>
2020-06-20 00:26:26 +0600

    Fixed the colors and drew the gameboard background

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ PL_RADIUS = 20
 PUCK_RADIUS = 10
 GOAL_WIDHT = 150
 GOAL_HEIGHT = 10
+background = pygame.image.load('gameBG.jpg')
 
 # making the goals as a rect object
 p1_goal = pygame.Rect(WIDTH // 2 - GOAL_WIDHT // 2, 0, GOAL_WIDHT, GOAL_HEIGHT)
@@ -33,6 +34,9 @@ p2_goal = pygame.Rect(WIDTH // 2 - GOAL_WIDHT // 2, HEIGHT - GOAL_HEIGHT, GOAL_W
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GOAL_COL = (102, 102, 255)
+P1_COL = (232, 38, 38)
+P2_COL = (70, 180, 88)
 
 
 def drawBoard():
@@ -40,13 +44,14 @@ def drawBoard():
     pygame.draw.line(win, WHITE, (0, HEIGHT // 2), (WIDTH, HEIGHT // 2))
     pygame.draw.circle(win, WHITE, (WIDTH // 2, HEIGHT // 2), 100, 1)
 
-    pygame.draw.rect(win, RED, p1_goal, 1)
-    pygame.draw.rect(win, RED, p2_goal, 1)
+    pygame.draw.rect(win, GOAL_COL, p1_goal, 3)
+    pygame.draw.rect(win, GOAL_COL, p2_goal, 3)
 
 
 def redraw():
     # function that redraws all the elements in the window
-    win.fill(BLACK)  # fill the screen to remove the last frame
+    # win.fill(BLACK)  # fill the screen to remove the last frame
+    win.blit(background, (0, 0))
     drawBoard()
     p1.draw(win)
     p2.draw(win)
@@ -55,8 +60,8 @@ def redraw():
 
 
 # instancing the objects for the game
-p1 = Player(100, 100, PL_RADIUS, PL_SPEED, WIDTH, 0, HEIGHT / 2, (255, 0, 0), 'Player_1')  # fix this harcoded issue and make it accessible by constants
-p2 = Player(300, 500, PL_RADIUS, PL_SPEED, WIDTH, HEIGHT / 2, HEIGHT, (0, 255, 0), 'PLayer_2')
+p1 = Player(100, 100, PL_RADIUS, PL_SPEED, WIDTH, 0, HEIGHT / 2, P1_COL, 'Player_1')  # fix this harcoded issue and make it accessible by constants
+p2 = Player(300, 500, PL_RADIUS, PL_SPEED, WIDTH, HEIGHT / 2, HEIGHT, P2_COL, 'PLayer_2')
 puck = Puck(WIDTH // 2, HEIGHT // 2, PUCK_RADIUS, WIDTH, HEIGHT, PUCK_SPEED, WHITE)
 
 run = True  # for running the main loop
